25a97732cf56f786646a5a20af62777c05a4de1e
Trican30403 <165995862+Trican30403@users.noreply.github.com>
2024-04-04 15:21:02 +0700

    add_file

---
+++ main.js
@@ -0,0 +1,6 @@
+function signup(e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+}
